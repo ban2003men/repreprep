656632403c26567b12a153ad3df4902b7e8dc535
ban2003men <90034882+ban2003men@users.noreply.github.com>
2024-09-26 23:00:37 +0300

    Update keyboards.py

--- Web_App-main/bot/keyboards.py
+++ Web_App-main/bot/keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
 
-web_app = WebAppInfo(url='site_url')
+web_app = WebAppInfo(url='https://github.com/ban2003men/repreprep')
 
 keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
